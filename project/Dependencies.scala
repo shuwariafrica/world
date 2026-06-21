1b16e56f82100c741dbd9668cab8f04674b3dc99
Ali Rashid <ali@shuwari.africa>
2026-06-22 00:59:03 +0300

    Update scala-java-time to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies:
   val boilerplate = "io.github.arashi01" %% "boilerplate" % "0.7.0"
   val munit = "org.scalameta" %% "munit" % "1.2.4"
   val `munit-scalacheck` = "org.scalameta" %% "munit-scalacheck" % "1.2.0"
-  val `scala-java-time` = "io.github.cquiroz" %% "scala-java-time" % "2.6.0"
+  val `scala-java-time` = "io.github.cquiroz" %% "scala-java-time" % "2.7.0"
   val `scala-java-time-tzdb` = `scala-java-time`.withName("scala-java-time-tzdb")
