6bb3099c0224e009893e4c94bade2406a41ed4c5
Ali Rashid <ali@shuwari.africa>
2026-06-22 00:52:10 +0300

    Correct code linting and formatting

--- modules/locale/src/test/scala/world/locale/format/locale_formatter_tests.scala
+++ modules/locale/src/test/scala/world/locale/format/locale_formatter_tests.scala
@@ -1,5 +1,5 @@
 /****************************************************************
- * Copyright © Shuwari Africa Ltd.                              *
+ * Copyright © 2023, 2026 Shuwari Africa Ltd.                   *
  *                                                              *
  * This file is licensed to you under the terms of the Apache   *
  * License Version 2.0 (the "License"); you may not use this    *
