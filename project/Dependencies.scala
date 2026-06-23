22798d24d2f05425a5f170659868a89062f3c871
Shuwari Africa Team <developers@shuwari.africa>
2026-06-23 07:15:43 +0000

    Update boilerplate to 0.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies:
   val scalaVersion = "3.8.4"
-  val boilerplate = "io.github.arashi01" %% "boilerplate" % "0.7.0"
+  val boilerplate = "io.github.arashi01" %% "boilerplate" % "0.8.2"
   val munit = "org.scalameta" %% "munit" % "1.3.3"
   val `munit-scalacheck` = "org.scalameta" %% "munit-scalacheck" % "1.2.0"
   val `scala-java-time` = "io.github.cquiroz" %% "scala-java-time" % "2.7.0"
