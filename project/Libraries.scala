fa961ee5384f46e464904a936d51609a62403abf
Shuwari Africa Team <developers@shuwari.africa>
2026-09-03 10:30:31 +0000

    [dependencies] Update munit to 1.3.6

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -3,7 +3,7 @@ import sbt.{Compile, Def, IO, *}
 
 object Libraries extends AutoPlugin:
   val scala3 = "3.9.0-RC6"
-  val munit = "org.scalameta" %% "munit" % "1.3.5"
+  val munit = "org.scalameta" %% "munit" % "1.3.6"
 
   // The ecosystem substrate world sits above: the typed-error base, the scalar wire-text contract,
   // the locale-free codec vocabulary, and the null-elimination utilities.
